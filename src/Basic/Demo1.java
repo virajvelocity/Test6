b5e422a132477c506ada74150d14364fa8cdfddc
VIRAJ <VIRAJ@DESKTOP-U6OK72A>
2022-10-29 08:46:09 +0530

    First Commit-I

---
+++ src/Basic/Demo1.java
@@ -0,0 +1,23 @@
+package Basic;
+
+public class Demo1
+{
+
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Good Morning");
+		
+		m1();
+	}
+	
+	public static void m1()
+	{
+		System.out.println("Hello");
+	}
+	
+	
+	
+	
+	
+}
